37599363d0c993cbab12cf734cf51d02235d6b4d
EmaciatedStudent <yana.laperdina@gmail.com>
2022-04-12 16:46:33 +0500

    started restaurants module creation

--- platform-backend/src/app.module.ts
+++ platform-backend/src/app.module.ts
@@ -4,7 +4,8 @@ import * as Joi from 'joi';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm'; 
-
+import { RestaurantsModule } from './restaurants/restaurants.module';
+import { Restaurant } from './restaurants/entities/restaurant.entity';
 
 @Module({
   imports: [
@@ -28,14 +29,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      synchronize: true,
-      logging: true
+      synchronize: process.env.NODE_ENV !== "prod",
+      logging: true,
+      entities: [Restaurant]
     }),
 
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
-    })
+    }),
+    RestaurantsModule
   ],
   controllers: [],
   providers: [],
